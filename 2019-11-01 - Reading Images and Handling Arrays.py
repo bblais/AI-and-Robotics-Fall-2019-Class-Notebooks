cd777bb8d8074e99ceb75aa79bff889206512386
Brian Blais <bblais@gmail.com>
2019-11-01 12:09:14 -0400

    another read state -- from file

---
+++ 2019-11-01 - Reading Images and Handling Arrays.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('pylab', 'inline')
+
+
+# In[10]:
+
+
+plot(rand(100))
+xlabel('this')
+ylabel('that')
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+arr=imread('images/test9.jpg')
+
+
+# In[12]:
+
+
+imshow(arr)
+
+
+# In[4]:
+
+
+arr.shape
+
+
+# In[13]:
+
+
+arr
+
+
+# In[8]:
+
+
+arr.shape
+
+
+# In[19]:
+
+
+L=[3,5,2,3,45,6,3,2,5]
+M=[3,5,2,3,45,6,3,2,5,8,9,2]
+
+
+# In[15]:
+
+
+L
+
+
+# In[16]:
+
+
+L[4]
+
+
+# In[17]:
+
+
+L[4:7]
+
+
+# In[20]:
+
+
+L[4:],M[4:]
+
+
+# In[31]:
+
+
+L[:4]
+
+
+# In[32]:
+
+
+L[:]  # beginning to end
+
+
+# In[21]:
+
+
+L[7:10]
+
+
+# In[22]:
+
+
+L[7:12]
+
+
+# In[ ]:
+
+
+
+
+
+# In[23]:
+
+
+L[12]
+
+
+# In[26]:
+
+
+arr_patch=arr[3:8,100:105,2]
+arr_patch
+
+
+# In[29]:
+
+
+imshow(arr_patch,cmap=cm.gray)
+colorbar()
+
+
+# In[39]:
+
+
+figure(figsize=(16,8))
+blue=arr[:,:,2]
+subplot(1,2,1)
+imshow(blue,cmap=cm.gray)
+colorbar()
+
+subplot(1,2,2)
+imshow(arr)
+
+
+# In[37]:
+
+
+figure(figsize=(16,8))
+red=arr[:,:,0]
+subplot(1,2,1)
+imshow(red,cmap=cm.gray)
+colorbar()
+
+subplot(1,2,2)
+imshow(arr)
+
+
+# In[40]:
+
+
+figure(figsize=(16,8))
+green=arr[:,:,1]
+subplot(1,2,1)
+imshow(green,cmap=cm.gray)
+colorbar()
+
+subplot(1,2,2)
+imshow(arr)
+
+
+# In[41]:
+
+
+get_ipython().run_line_magic('pylab', 'inline')
+
+
+# In[46]:
+
+
+pwd
+
+
+# In[42]:
+
+
+arr=imread('images/bird.jpg')
+
+
+# In[43]:
+
+
+imshow(arr)
+
+
+# In[44]:
+
+
+arr.shape
+
+
+# In[50]:
+
+
+red=arr[:,:,0]
+green=arr[:,:,1]
+blue=arr[:,:,2]
+
+figure(figsize=(16,8))
+subplot(1,2,1)
+imshow(green/255,cmap=cm.gray)
+colorbar()
+
+subplot(1,2,2)
+imshow(arr)
+
+
+# In[ ]:
+
+
+
+
---
+++ 2019-11-1 - Read State from File.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from Game import *
+
+
+# In[2]:
+
+
+b=Board(4,4)
+
+
+# In[14]:
+
+
+def read_state():
+    with open('example state.txt') as fid:
+        text=fid.read()
+
+    text2=text.strip().split('\n')
+    number_of_rows=len(text2)
+    number_of_cols=len(text2[0].split())
+    
+    b=Board(number_of_rows,number_of_cols)
+        
+    board=[int(v) for v in text.split()]
+    b.board=board
+    return b
+
+
+# In[15]:
+
+
+state=read_state()
+print(state,state.shape)
+
+
+# In[19]:
+
+
+with open('example state.txt') as fid:
+    text=fid.read()
+
+text2=text.strip().split('\n')
+text3=text.split('\n')
+
+
+# In[17]:
+
+
+text
+
+
+# In[18]:
+
+
+text2
+
+
+# In[20]:
+
+
+text3
+
+
+# In[10]:
+
+
+text.split()
+
+
+# In[ ]:
+
+
+
+
